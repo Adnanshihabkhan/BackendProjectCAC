41f5086ed87bf4097de28e861ed655a3d08b4f49
Adnanshihabkhan <adnanshihabkhan@gmail.com>
2025-05-31 11:15:21 +0530

    debug conroller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -328,16 +328,55 @@ const getUserChannelPorfile = asyncHandler(async (req, res) => {
         from: "subscriptions",
         localField: "_id",
         foreignField: "channel",
+        as: "subscribers",
       },
     },
     {
       $lookup: {
         from: "subscriptions",
         localField: "_id",
-        foreignField: "",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
       },
     },
   ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "User channel fetched succesfully"));
 });
 
 export {
